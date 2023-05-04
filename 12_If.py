ea01ef025e947e012f8ba07a701f64e4053a2a0a
danakim530 <danakim530@gmail.com>
2023-05-04 22:41:49 +0900

    12_If.py

---
+++ 12_If.py
@@ -0,0 +1,10 @@
+input_name = 'Dana'
+
+if input_name == 'Dana':
+    print('만나서 반가워요,', input_name)
+elif input_name == 'Tana':
+    print('타나 반가워요')
+elif input_name == 'Meta':
+    print('당신이 Meta군요!')
+else:
+    print('당신은?!!')
