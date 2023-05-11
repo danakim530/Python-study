e9b5e905cf2bd1c484537d53bd65cdf34dd812c0
danakim530 <danakim530@gmail.com>
2023-05-11 23:55:28 +0900

    07_mytuple packing unpacking

--- 05_Tupple.py
+++ 05_Tupple.py
@@ -8,3 +8,8 @@ my_tuple = (1, 2, 3)
 your_tuple = 1, 2, 3  # 괄호 없이 써도 자동으로 튜플되어 있음
 
 # Packing(값을 묶기) vs Unpacking(묶여있는 값을 풀기)
+my_tuple = 1, 2, 3  # 1,2,3 값을 튜플로 묶음 packing
+num1, num2, num3 = my_tuple  # 각각 알아서 값이 들어감 unpacking
+print(num1, num2, num3)
+
+num1, num2 = num2, num1  # 값을 서로 바꿀 수 있음. 오른쪽에서 packing 되고, 왼쪽에서 unpacking 된 것
