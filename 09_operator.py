26ccaa2cce8fe1d7aee25e381d34ad72aca382d4
danakim530 <danakim530@gmail.com>
2023-05-12 23:49:08 +0900

    09_operator.py

---
+++ 09_operator.py
@@ -0,0 +1,23 @@
+# Operator 연산자
+# Assign 할당 연산자
+count = 1
+print(count)
+
+# 복합 할당 연산자
+count += 1
+count -= 1
+count *= 2
+count /= 3
+
+# Arithmetic 산술 연산자 (숫자끼리)
+# + - * /
+
+# 특수 연산자
+# ** : 제곱
+# // : 몫
+# % : 나머지
+
+print(3**2)  # 3의 제곱
+print(7/3)  # 2.333
+print(7 // 3)  # 몫이 2
+print(7 % 3)  # 나머지가 1
