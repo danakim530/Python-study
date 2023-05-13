9a93d3d9bd0154b9792c2318726e5567532ef6a2
danakim530 <danakim530@gmail.com>
2023-05-14 00:49:40 +0900

    methods.py

--- Methods.py
+++ Methods.py
@@ -8,3 +8,17 @@ str.lower()  # 소문자로
 
 # 두 정수 중 큰 수 뱉기
 max(A, B)
+
+# 더하기
+sum(a, b)
+
+# 길이 len()
+
+# 문자열 합치기 join() - 더하기 안해도 됨. 한 줄로 끝남
+words = ["안녕", "나는", "다나"]
+result = ' '.join(words)
+print(result)
+
+# 문자열 합치기 append()
+
+# if, if not
