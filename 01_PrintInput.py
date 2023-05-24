4be60f2943c820ad8fdf606e1e39d5c2c4e334be
Dana Kim <124070996+danakim530@users.noreply.github.com>
2023-05-24 23:07:31 +0900

    Update 01_PrintInput.py

--- 01_PrintInput.py
+++ 01_PrintInput.py
@@ -1,7 +1,7 @@
 # Python 설치 (python.org)-> python 3.x.x 버전으로 시작하는 것을 추천
 # IDLE Shell이 생김
 # 실행 : vscode 위의 세모 누르기(ctrl + f5)
-
+#
 # 출력__________________________________
 print("hello world")
 print(1)
